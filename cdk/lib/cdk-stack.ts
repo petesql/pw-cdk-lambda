f4b3f9c1f5c8ae10c65ca7d33a1649effeeb6f72
pete <peter_whyte@outlook.com>
2021-04-28 00:12:56 +0100

    feat: add create user with read only pol

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,9 +1,15 @@
 import * as cdk from '@aws-cdk/core';
+import { ManagedPolicy, User } from '@aws-cdk/aws-iam';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const user = new User(this, 'simpleuser', {
+      userName: 'pw-simple-user',
+      managedPolicies: [ 
+        ManagedPolicy.fromManagedPolicyArn(this,'ropol', 'arn:aws:iam::aws:policy/ReadOnlyAccess'),
+      ]
+    });
   }
 }
